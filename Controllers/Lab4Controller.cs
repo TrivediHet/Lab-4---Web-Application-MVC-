bb17d4372d5cb57f3b6a92bef45a6929e725ab4e
Het Trivedi <35612465+TrivediHet@users.noreply.github.com>
2018-02-19 20:56:35 -0500

    Add files via upload

---
+++ Controllers/Lab4Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Lab4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            DateTime today = DateTime.Now;
+            ViewData["hour"] = today.Hour;
+            ViewData["shortTime"] = today.ToShortTimeString();
+            ViewData["longDate"] = today.ToLongDateString();
+            ViewData["days"] = (new DateTime(today.Year+1, 1, 1)).Subtract(today).Days;
+            ViewData["year"] = today.Year;
+            return View();
+        }
+
+        public IActionResult Page2(int id)
+        {
+            return View(id);
+        }
+
+        public IActionResult Page3()
+        {
+            ViewData["header"] = "Beverages";
+            String[] list = { "Tea", "Coffee", "Soda", "Beer", "Wine" };
+            return View(list);
+        }
+    }
+}
